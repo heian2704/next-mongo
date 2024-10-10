3ad6492d2ba3f8faf56645095f14174f92dc8863
heian2704 <heinthant2704@gmail.com>
2024-10-10 15:12:20 +0700

    first commit

--- app/product/page.js
+++ app/product/page.js
@@ -4,6 +4,11 @@ import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+<<<<<<< HEAD
+=======
+  const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+  console.debug("API_BASE", API_BASE);
+>>>>>>> 6d71485 (Print APIBASE)
   const { register, handleSubmit } = useForm();
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState([]);
